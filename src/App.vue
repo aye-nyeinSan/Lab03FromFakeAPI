a2bcc5be298719eeb1159a32999c8fc21c1e155d
aye-nyeinSan <aye2904@gmail.com>
2024-08-03 11:54:26 +0700

    added message state

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router';
+  import { useMessageStore } from '@/stores/message';
+  import { storeToRefs } from 'pinia';
+  const store = useMessageStore();
+  const { message } = storeToRefs(store);
 </script>
 
 <template>
-
- 
-<RouterView></RouterView>
+  <div id="layout">
+    <div id="flashMessage" v-if="message">
+      <h4>{{ message }}</h4>
+    </div>
+    <RouterView></RouterView>
+  </div>
 </template>
 
 <style scoped>
-
+  #layout {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 2rem;
+    color: #2c3e50;
+  }
+  @keyframes yellowfade {
+    from {
+      background-color: yellow;
+    }
+    to {
+      background-color: transparent;
+    }
+  }
+  #flashMessage {
+    animation: yellowfade 3s ease-in-out;
+  }
 </style>
--- src/views/Passenger/DetailView.vue
+++ src/views/Passenger/DetailView.vue
@@ -2,6 +2,9 @@
   import { toRefs, defineProps } from 'vue';
   import { type Passenger } from '@/types';
 
+  import { useMessageStore } from '@/stores/message';
+  import router from '@/router';
+  const store = useMessageStore();
   const props = defineProps<{
     passenger: Passenger;
     id: string;
@@ -9,13 +12,30 @@
   }>();
 
   const { passenger } = toRefs(props);
+  const update = () => {
+    console.log('Update');
+    store.setMessage('Passenger Updated: ' + props.passenger.name);
+    setTimeout(() => {
+      store.setMessage('');
+    }, 3000);
+    router.push({
+      name: 'passenger-list-view',
+    });
+  };
 </script>
 
 <template>
-  <p>
-    {{ passenger?.name }} on airline {{ passenger?.airline[0].name }} @
-    {{ passenger?.airline[0].country }} country
-  </p>
-  <p><img :src="passenger?.airline[0].logo" alt="Airline Logo" /></p>
-  <RouterLink :to="{  name: 'passenger-list-view'}">Back To Home</RouterLink>
+  <div>
+    <h1> Passenger Detail</h1>
+    <p>
+      {{ passenger?.name }} on airline {{ passenger?.airline[0].name }} @
+      {{ passenger?.airline[0].country }} country
+    </p>
+    <p>
+      <img :src="passenger?.airline[0].logo" alt="Airline Logo" />
+    </p>
+    <button @click="update">Update</button>
+  </div>
+
+  <RouterLink :to="{ name: 'passenger-list-view' }">Back To Home</RouterLink>
 </template>
